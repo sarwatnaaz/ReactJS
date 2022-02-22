c38b90f57203f5669a04910f3acc300fce235555
sarwatnaaz <99759579+sarwatnaaz@users.noreply.github.com>
2022-02-22 09:24:49 -0500

    Add files via upload

---
+++ WorkingWithLet.js
@@ -0,0 +1,21 @@
+function getData() {
+    let name = "Sarwat";
+    console.log(name)
+
+    for(let i=0; i<10; i++){
+        console.log(i)
+    }
+
+    if(name == "Sarwat"){
+        let age = 23;
+        console.log(age);
+    }
+
+    console.log(age);
+    console.log(i);
+    
+
+    debugger;
+}
+
+getData()
