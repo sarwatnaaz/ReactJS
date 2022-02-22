ff8069eecb7165422dc00ba569814da9bf26e3fb
sarwatnaaz <99759579+sarwatnaaz@users.noreply.github.com>
2022-02-22 10:12:03 -0500

    Add files via upload

---
+++ React/src/components/HeaderComponents.js
@@ -0,0 +1,8 @@
+export default function HeaderComponents(){
+    return(
+      <div>
+        <h1>This is Header for Session...</h1>
+        <h2>This is other header...</h2>
+      </div>
+    )
+  }
