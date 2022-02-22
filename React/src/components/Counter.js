5c0c0c037f0db6aa56822a7c641dfd6464e0b3a0
sarwatnaaz <99759579+sarwatnaaz@users.noreply.github.com>
2022-02-22 10:11:17 -0500

    Create Counter.js

---
+++ React/src/components/Counter.js
@@ -0,0 +1,14 @@
+export default function Counter(props) {
+    return <h1>Counter Value is: {props.count}</h1>
+}
+
+// export default function Counter() {
+//     var i = 0 ;
+
+//     setInterval(function() {
+//         i = i + 1;
+//         console.log("Counter at : "+i);
+//     },1000)
+// }
+
+//<h1>Counter value: {i}</h1>
